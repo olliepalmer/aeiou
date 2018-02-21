703955fe7f799c24ba37a836b93fa6f20c0edee2
olliepalmer <github@olliepalmer.com>
2018-02-21 20:00:40 +0100

    Updated python code
    Include Google credentials, and prevent from searching Google twice per
    translation!

--- translate_and_tweet.py
+++ translate_and_tweet.py
@@ -1,3 +1,5 @@
+import os
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "credentials.json"
 from google.cloud import translate
 import json
 import random
@@ -11,22 +13,17 @@ file_path = '/Users/o/Dropbox/Apps/ifttt/translationbot/' # this is the path to 
 
 def makePhrase():
 	phrase = ''
-	for i in range(250):
+	for i in range(random.randint(150,250)):
 		phrase += random.choice(letters)
+	print ('created phrase: \n'+phrase)
 	return(phrase)
 
 def translateJibberish():
 	translation = translate_client.translate(
 		makePhrase(),
 		target_language=target)
+	print('done!')
 	return(translation)
-
-print (translateJibberish())
-
-
-
-
-
 
 def tweet():
 	the_data = translateJibberish()
@@ -34,13 +31,16 @@ def tweet():
 	json_to_python = json.loads(data)
 
 	the_text = json_to_python['translatedText']
-	f = open(file_path+the_text+".txt", "w+")
-	f.write(str(json_to_python))
-	f.close()
+	print('saving file as: '+the_text+".txt")
+	with open(os.path.join(file_path,the_text+".txt"), "w+") as outfile:
+		json.dump(json_to_python,outfile)
+	# f = open(os.path.join(file_path,the_text+".txt"), "w+")
+	# f.write(str(json_to_python))
+	# f.close()
 	# nb this is saved into dropbox
 	# and will be tweeted via ifttt
 	# using workflow which tweet the title of every
 	# new text file within a specified folder 
-
+	print ('done!')
 
 tweet()
