0db40a52621d58b656118c1f8444e150fc2609a4
olliepalmer <github@olliepalmer.com>
2018-05-11 12:14:21 +0200

    added wisdom.txt file, amended stripped down version

--- stripped-down-version.py
+++ stripped-down-version.py
@@ -6,12 +6,12 @@ letters = ['a','e','i','o','u',' ']
 
 def impart_wisdom():
 	input_phrase = '' # reset the phrase
-	for i in range(random.randint(150,250)):
+	for i in range(random.randint(50,300)):
 		input_phrase += random.choice(letters)
 	translation = ancient_wisdom.translate(input_phrase, target_language='en') # this sends google our phrase and receives their translation
-	advice = json.dumps(translation) # but the translation in in a format called json, which sounds just like 'jason' but is a way of storing data
+	advice = json.dumps(translation).encode('utf8') # but the translation in in a format called json, which sounds just like 'jason' but is a way of storing data
 	make_legible = json.loads(advice) # we need to get data out of the json format and into plain text
-	return(make_legible['input'],make_legible['translatedText'])
+	with open('wisdom.txt','a') as file:
+		file.write(make_legible['input'] + '\n' + make_legible['translatedText'] + '\n\n')
 
-wisdom = impart_wisdom()
-print(wisdom[0],'/',wisdom[1])
+impart_wisdom()
